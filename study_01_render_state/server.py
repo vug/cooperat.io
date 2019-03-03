5dc33e86adfa2238ceef2655e982e646c2bb95b6
Ugur Guney <ugurguney@gmail.com>
2019-03-03 01:58:13 -0500

    Canvas renders game state.
    - Demote Unit class to dict for simpler JSON serialization.
    - Initialize with 10 units at random coordinates.
    - Game loop computes new coordinates of untis and updates `game_state`
    - `serve()` broadcasts game state (a JSON serialized dict) to all clients.
    - 40 FPS.
    - New canvas element in client.
    - onmessage clear canvas
    - onmessage draw a green rectangle at each unit coordinate.

--- study_01_render_state/server.py
+++ study_01_render_state/server.py
@@ -1,34 +1,35 @@
 import asyncio
 import datetime
+import json
 import functools
 import random
 
 import websockets
 
 
-class Unit(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
 async def game_loop(game_state):
     while True:
         # update game state
-        print(datetime.datetime.utcnow().isoformat(), game_state)
-        await asyncio.sleep(0.5)
+        for u in game_state["units"]:
+            u["x"] = (u["x"] + 0.05 * random.random() - 0.025) % 10
+            u["y"] = (u["y"] + 0.05 * random.random() - 0.025) % 10
+        game_state["ts"] = datetime.datetime.utcnow().isoformat()
+        await asyncio.sleep(0.025)
 
 
 async def serve(ws, path, game_state):
-    print(f"game state: {game_state}")
+    print(f"client connected: {ws.remote_address}")
     while True:
-        now = datetime.datetime.utcnow().isoformat() + "Z"
-        await ws.send(now)
-        await asyncio.sleep(random.random() * 3)
+        await ws.send(json.dumps(game_state))
+        await asyncio.sleep(0.025)
 
 
-async def all_routines():
-    gs = {"units": []}
+async def run_all():
+    gs = {"units": [], "ts": None}
+    n_units = 10
+    for _ in range(n_units):
+        u = {"x": random.random() * 10, "y": random.random() * 10}
+        gs["units"].append(u)
 
     start_server = websockets.serve(
         functools.partial(serve, game_state=gs), "localhost", 8765
@@ -39,5 +40,5 @@ async def all_routines():
 if __name__ == "__main__":
 
     event_loop = asyncio.get_event_loop()
-    event_loop.run_until_complete(all_routines())
+    event_loop.run_until_complete(run_all())
     event_loop.run_forever()
