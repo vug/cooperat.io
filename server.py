02a33f89bffb7cc1dc32b16b045e36cc30c4181f
Ugur Guney <ugurguney@gmail.com>
2019-03-04 03:05:01 -0500

    Refactor, rename.

--- server.py
+++ server.py
@@ -9,32 +9,44 @@ import websockets
 
 
 async def game_loop(game_state):
+    """Update game state periodically.
+
+    This happens independent of client connections, hence no websocket references are 
+    needed.
+    """
     while True:
         ts_prev = game_state["ts"]
         ts_curr = datetime.datetime.utcnow().timestamp()
         delta_t = ts_curr - ts_prev
-        for u in game_state["units"]:
-            u["x"] = (u["x"] + math.cos(u["dir"]) * u["speed"] * delta_t) % 10
-            u["y"] = (u["y"] + math.sin(u["dir"]) * u["speed"] * delta_t) % 10
-            u["dir"] = (u["dir"] + random.random() * 0.4 - 0.2) % (2.0 * math.pi)
+
+        update_positions(game_state, delta_t)
+
         game_state["ts"] = datetime.datetime.utcnow().timestamp()
         game_state["tick_no"] += 1
         await asyncio.sleep(0.025)
 
 
-async def serve(ws, path, game_state):
+def update_positions(game_state, delta_t):
+    for u in game_state["units"]:
+        u["x"] = (u["x"] + math.cos(u["dir"]) * u["speed"] * delta_t) % 10
+        u["y"] = (u["y"] + math.sin(u["dir"]) * u["speed"] * delta_t) % 10
+        if u["type"] == "ghost":
+            u["dir"] = (u["dir"] + random.random() * 0.4 - 0.2) % (2.0 * math.pi)
+
+
+async def handler(ws, path, game_state):
     print(f"client connected: {ws.remote_address}")
     while True:
         await ws.send(json.dumps(game_state))
         await asyncio.sleep(0.025)
 
 
-async def run_all():
+async def run_server():
     game_state = init_game_state()
     game_loop_routine = game_loop(game_state)
-    ws_server_routine = websockets.serve(
-        functools.partial(serve, game_state=game_state), "localhost", 8765
-    )
+    # Add an argument to handler
+    bound_handler = functools.partial(handler, game_state=game_state)
+    ws_server_routine = websockets.serve(bound_handler, "localhost", 8765)
     await asyncio.gather(game_loop_routine, ws_server_routine)
 
 
@@ -43,6 +55,7 @@ def init_game_state():
     n_units = 10
     for _ in range(n_units):
         u = {
+            "type": "ghost",
             "x": random.random() * 10,
             "y": random.random() * 10,
             "speed": 1.0,
@@ -54,5 +67,5 @@ def init_game_state():
 
 if __name__ == "__main__":
     event_loop = asyncio.get_event_loop()
-    event_loop.run_until_complete(run_all())
+    event_loop.run_until_complete(run_server())
     event_loop.run_forever()
