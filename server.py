f9a84bb5f61d75de8fd5483a0f1d85dec3a58c3c
Ugur Guney <ugurguney@gmail.com>
2019-03-04 23:51:10 -0500

    Make units a `dict`. Remove disconnected client units by lookup.

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ connectedSockets = {}
 
 class GameState(object):
     def __init__(self):
-        self.units = []
+        self.units = {}
         self.ts = datetime.datetime.utcnow().timestamp()
         self.tick_no = 0
         self.num_units_created = 0
@@ -36,7 +36,7 @@ def game_loop(game_state):
 
 
 def update_positions(game_state, delta_t):
-    for u in game_state.units:
+    for id, u in game_state.units.items():
         u["x"] = (u["x"] + math.cos(u["dir"]) * u["speed"] * delta_t) % 10
         u["y"] = (u["y"] + math.sin(u["dir"]) * u["speed"] * delta_t) % 10
         if u["type"] == "ghost":
@@ -49,7 +49,8 @@ async def producer_handler(ws, path, game_state):
             game_loop(game_state)
             message = {"ts": game_state.ts, "tick_no": game_state.tick_no}
             message["units"] = [
-                {k: u[k] for k in ["type", "x", "y", "dir"]} for u in game_state.units
+                {k: u[k] for k in ["type", "x", "y", "dir"]}
+                for u in game_state.units.values()
             ]
 
             await ws.send(json.dumps(message))
@@ -57,11 +58,9 @@ async def producer_handler(ws, path, game_state):
     except websockets.ConnectionClosed:
         logging.info(f"User {ws.remote_address} has left.")
     finally:
-        us = [u for u in game_state.units if u.get("id") == connectedSockets[ws]][0]
-        logging.info(
-            f"Removing {ws.remote_address}'s unit. id: {connected[ws]}, us: {us}"
-        )
-        game_state.units.remove(us)
+        uid = connectedSockets[ws]
+        logging.info(f"Removing {ws.remote_address}'s unit. id: {uid}")
+        game_state.units.pop(uid, None)
         connectedSockets.pop(ws, None)
 
 
@@ -78,11 +77,10 @@ async def connection_handler(ws, path, game_state):
         "y": random.random() * 10,
         "speed": 1.0,
         "dir": 0,
-        "id": uid,
     }
-    logging.info(f"client connected: {ws.remote_address}. unit: f{u}")
+    logging.info(f"client connected: {ws.remote_address}. Given id: {uid}.")
     connectedSockets[ws] = uid
-    game_state.units.append(u)
+    game_state.units[uid] = u
     game_state.num_units_created += 1
     producer_task = asyncio.create_task(producer_handler(ws, path, game_state))
     consumer_task = asyncio.create_task(consumer_handler(ws, path, game_state))
@@ -106,14 +104,15 @@ def init_game_state():
     gs = GameState()
     n_ghosts = 10
     for _ in range(n_ghosts):
-        u = {
+        unit = {
             "type": "ghost",
             "x": random.random() * 10,
             "y": random.random() * 10,
             "speed": 1.0,
             "dir": random.random() * math.pi,
         }
-        gs.units.append(u)
+        uid = gs.num_units_created
+        gs.units[uid] = unit
         gs.num_units_created += 1
     return gs
 
